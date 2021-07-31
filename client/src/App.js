18cbf7e9fa79f98d89c12781ab6038088301a50f
Edward Pedroza <epedroza@purdue.edu>
2021-07-30 20:16:04 -0500

    added Tabs for Basketball and COD

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import Tabs from "./components/Tabs";
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <Tabs>
+        <div label="Basketball Power Rankings">
+
+        </div>
+        <div label="COD Power Rankings">
+
+        </div>
+      </Tabs>
       <header className="App-header">
         <p>
           Tuskerdoo Crew Power Rankings - Coming Soon
         </p>
+        
       </header>
     </div>
   );
